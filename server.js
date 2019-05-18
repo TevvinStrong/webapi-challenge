c0ddfceed25db1c4d61dc273bc40a57cba04d7f9
Tevvin <Tevvin7395@icloud.com>
2019-05-18 13:36:07 -0400

    added route handlers and routes file for actions and project

--- server.js
+++ server.js
@@ -4,18 +4,22 @@ const helmet = require('helmet');
 const logger = require('morgan');
 
 // Routes-endpoints
-const actionModel = require('./data/helpers/actionModel.js');
-const projectModel = require('./data/helpers/projectModel.js');
+const actionModel = require('./routers/actions-model.js');
+const projectModel = require('./routers/project-model.js');
 
 const server = express();
 
 // Configure global middleware
 server.use(express.json());
 server.use(helmet());
-//TODO: Once MVP is complete add logger middleware
 server.use(logger('dev'));
 
+// Configure route handlers
+server.use('/api/actions', actionModel);
+server.use('/api/projects', projectModel);
 
+
+// Test.
 server.get('/', (req, res) => {
     res.send('<h1>This is a test>/h1>');
 });
